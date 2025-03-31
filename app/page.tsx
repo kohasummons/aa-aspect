64ddd2122dd2c3f3841419880a66f25436bbc9a5
Joshua Omobola <omobolathejoshua@gmail.com>
2025-03-31 11:42:33 +0100

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import CtaSection from "./components/sections/CtaSection";
 import ServicesSection from "./components/sections/ServicesSection";
 import BlogSection from "./components/sections/NewsSection";
 import Footer from "./components/layout/Footer";
-import TrustedBySection from "./components/sections/TrustedBySection";
 import { InfiniteScrollSection } from "./components/sections/InfiniteScrollSection";
 
 export default function Home() {
