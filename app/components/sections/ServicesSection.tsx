7cfbae2caba2a9a0dd621cec6abffd5be6b2f64b
Mercy Thaddeus <mercythaddeus234+github@gmail.com>
2025-11-19 13:35:36 +0100

    comment

--- app/components/sections/ServicesSection.tsx
+++ app/components/sections/ServicesSection.tsx
@@ -69,6 +69,7 @@ const ServicesSection = () => {
                 </div>
               </div>
             </Link>
+            {/* Process Engineering */}
 
             <Link
               href="/services/process-engineering"
